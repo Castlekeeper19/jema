e7d9a60d447d7c177a2db36088402e208c205420
Luke Chatelain <jens.chatelain@gmail.com>
2021-07-12 21:40:25 +0900

    updated terms, added to controller

--- app/controllers/churches_controller.rb
+++ app/controllers/churches_controller.rb
@@ -1,10 +1,12 @@
 class ChurchesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_church, only: [:show, :edit, :update]
 
   def index
     if params[:query].present?
       # @churches = Church.all.search_by_location(params[:query])
       @churches = Church.near(params[:query], 10)
+
     else
       @churches = Church.all
     end
@@ -19,7 +21,7 @@ class ChurchesController < ApplicationController
   end
 
   def show
-    @church = Church.find(params[:id])
+
     @markers = [
       {
         lat: @church.latitude,
@@ -28,8 +30,40 @@ class ChurchesController < ApplicationController
   end
 
   def new
+    @church = Church.new
+
+  end
+
+  def create
+    @church = Church.new(church_params)
+    if @church.save
+      redirect_to churches_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
   end
 
   def update
+    if @church.update(church_params)
+      redirect_to church_path(@church)
+      else
+        render :edit
+    end
+  end
+
+
+
+  private
+
+  def set_church
+    @church = Church.find(params[:id])
+  end
+
+  def church_params
+    params.require(:church).permit(:name, :location)
   end
 end
