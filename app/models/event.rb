9d36f4c8c0d8f365ae8a15420f51404bd082c4f4
Luke Chatelain <jens.chatelain@gmail.com>
2021-07-17 11:39:34 +0900

    added stock images and events page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :church
+
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require 'date'
 puts 'clearing database'
 # Booking.destroy_all
 # Workout.destroy_all
-Church.destroy_all
+# Church.destroy_all
 puts 'Creating Churches'
 cities = ['Omotesando', 'Akihabara', 'Fujisawa', 'Saitama', 'Setagaya', 'Ueno', 'Meguro', 'Shibuya', 'Shinagawa', 'Kanda', 'Shinjuku', 'Mitaka']
 
@@ -19,19 +19,31 @@ cities = ['Omotesando', 'Akihabara', 'Fujisawa', 'Saitama', 'Setagaya', 'Ueno', 
 
 # puts "Created #{User.count} Users"
 
-20.times do
-    city = cities.sample
-    club = Church::CHURCHNAMES.sample
-    Church.create!(
-    address1: (101...499).to_a.sample,
-    city: "#{city}",
-    prefecture: "Tokyo",
-    location: ("#{city}, Tokyo"),
-    post_code: (((100..200).to_a.sample.to_s + "-" + (4000..6000).to_a.sample.to_s)),
-    name:   ("#{city} Christian #{club}"),
-    pastor: Faker::Name.first_name + " " + Faker::Name.last_name,
-    website: "www.#{city.downcase}#{club.downcase}.com"
-  )
-end
+# 20.times do
+#     city = cities.sample
+#     club = Church::CHURCHNAMES.sample
+#     Church.create!(
+#     address1: (101...499).to_a.sample,
+#     city: "#{city}",
+#     prefecture: "Tokyo",
+#     location: ("#{city}, Tokyo"),
+#     post_code: (((100..200).to_a.sample.to_s + "-" + (4000..6000).to_a.sample.to_s)),
+#     name:   ("#{city} Christian #{club}"),
+#     pastor: Faker::Name.first_name + " " + Faker::Name.last_name,
+#     website: "www.#{city.downcase}#{club.downcase}.com"
+#   )
+# end
 
 puts "Created #{Church.count} Churches"
+
+20.times do
+    church = Church.all.sample
+    user = User.all.sample
+    Event.create!(
+    church_id: church.id,
+    user_id: user.id,
+    location: church.location,
+    start: DateTime.now + ((7..31).to_a.sample),
+    name:   "Church Gathering"
+  )
+end
