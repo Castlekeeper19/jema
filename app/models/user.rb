64f3130a3f6ea7932d8ec5ab562b59c267fa5bcd
Luke Chatelain <jens.chatelain@gmail.com>
2022-02-07 22:26:12 +0900

    added password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,31 @@ class User < ApplicationRecord
   has_many :events
   has_many :bookings
   has_many :churches
+
+  validates :email, format: { with: /\A.*@.*\..*\z/ }
+  validates :email, uniqueness: true, on: :create
+
+  PASSWORD_FORMAT = /\A
+  (?=.{8,})          # Must contain 8 or more characters
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+  (?=.*[A-Z])        # Must contain an upper case character
+  (?=.*[[:^alnum:]]) # Must contain a symbol
+/x
+
+validates :password,
+  presence: true,
+  length: { in: Devise.password_length },
+  format: { with: PASSWORD_FORMAT },
+  confirmation: true,
+  on: :create
+
+validates :password,
+  allow_nil: true,
+  length: { in: Devise.password_length },
+  format: { with: PASSWORD_FORMAT },
+  confirmation: true,
+  on: :update
+
+
 end
